a02003e998c9c9e8dce3b40b88da70309c79f8fc
Oscar España <espaaoscar@gmail.com>
2021-03-26 22:29:46 -0500

    Creació del Navbar y uso del componente Link de next

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../components/NavBar/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <h1>Hola Mundo</h1>
     </div>
   );
