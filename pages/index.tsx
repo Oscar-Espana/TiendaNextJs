1a03cabba14175db806dee958446015b5077ac82
Oscar España <espaaoscar@gmail.com>
2021-04-02 00:06:29 -0500

    Agrefar el listado de aguacates en la pagina de inicio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 
 const Home = () => {
+
+  const [productList, setProductList] = useState <TProduct[]>([])
+
+  useEffect(() => {
+    window
+    .fetch('api/avo')
+    .then(response => response.json())
+    .then(({data, length}) =>{
+      setProductList(data)
+    })
+  }, [])
+
   return (
     <div>
       <Navbar />
       <h1>Hola Mundo</h1>
+      {
+        productList.map( (product, key)=>(
+          <div key={key} >{product.name}</div>
+        ))
+      }
     </div>
   );
 };
