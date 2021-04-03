cd0b96c81f505ad2f477773e404a41d9ef61282f
Oscar España <espaaoscar@gmail.com>
2021-04-03 18:06:17 -0500

    Ejemplo de como añadir CSS en JSX/TSX

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from '@components/Navbar/Navbar'
-import styles from './layout.module.css'
 
 const Layout:React.FC = ({children}) => {
     return (
-        <div className={styles.container} >
+        <div>
             <Navbar/>
             {children}
-            <footer>Mi footer</footer>
+            <footer className="container" >Mi footer</footer>
+            <style jsx>{`
+                .container{
+                    background:salmon;
+                }
+            `}
+            </style>
         </div>
     )
 }
