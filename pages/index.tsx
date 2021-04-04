5467f270ba0d6b2f995b06c8a3c3e3acf8193e83
Oscar España <espaaoscar@gmail.com>
2021-04-03 22:54:09 -0500

    Agregar estilos al listado de productos / aguacates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import {Row, Col, Card, Button} from 'react-bootstrap'
+import Link from 'next/link'
 
 const Home = () => {
 
@@ -16,11 +18,27 @@ const Home = () => {
   return (
     <div>
       <h1>Hola Mundo</h1>
+      <Row>
       {
         productList.map( (product, key)=>(
-          <div key={key} >{product.name}</div>
+          <Col xs={12} md={6} lg={4} key={key} className="d-flex justify-content-center">
+            <Card className="mb-4" style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={product.image} />
+              <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>
+                  ${product.price}
+                </Card.Text>
+                <Link href={`/product/${product.id}`}>
+                  <Button as='a' variant="primary">Seleccionar</Button>
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
         ))
       }
+      </Row>
+      
     </div>
   );
 };
